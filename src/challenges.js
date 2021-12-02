0895b83839e028f3e88e5b1bf508bee06a2c9945
RamosFonseca <beidseitig@gmail.com>
2021-12-02 16:36:42 -0300

    Desafios 1-5

--- src/challenges.js
+++ src/challenges.js
@@ -1,32 +1,49 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(a, b) {
+  if (a === true && b === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base*height)/2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase) {
+  let splitPhrase = phrase.split(' ');
+  return splitPhrase;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(list) {
+  let listExtremes = list[list.length -1] + ', ' + list[0];
+  return listExtremes;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins*3 + ties*1;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers) {
+  let highNumberIndex = 0;
+  let highNumber = 0;
+  let contHighNumber = 0;
+
+  for (let index of numbers) {
+    if (numbers[highNumberIndex] < numbers[index]) {
+      highNumber =numbers[index];
+    }
+    return highNumber;
+  }
+  
 }
+console.log(highestCount[9, 1, 2, 3, 9, 5, 7]);
 
 // Desafio 7
 function catAndMouse() {
